c71d6298c041f13a0f4dab34d2fe163e581c7cd4
Kurt V Grothe <kurt.grothe@outlook.com>
2023-01-13 16:02:21 -0800

    fix app.py def predict

--- app.py
+++ app.py
@@ -16,11 +16,7 @@ def index():
 
 
 def predict():
-    Sepal_Length = request.form['sepal_length']
-    Sepal_Width = request.form['sepal_width']
-    Petak_Length = request.form['petal_length']
-    Petal_Width = request.form['petal_width']
-    Sepal-perimeter_worst = request.form['perimeter_worst']
+    perimeter_worst = request.form['perimeter_worst']
     radius_worst  = request.form['radius_worst']
     concave_points_mean = request.form['concave_points_mean']
     area_worst = request.form['area_worst']
